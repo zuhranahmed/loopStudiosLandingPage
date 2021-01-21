3e1ae00ebe8ea3ea68726d87c66fc8cd98cec9ac
zuhranahmed <zuhranahmed@gmail.com>
2021-01-21 22:49:41 +0500

    fix minor scroll issue

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const openMenu = document.querySelector(".burger-menu");
 const closeMenu = document.querySelector(".close-menu");
 const mobileNav = document.querySelector(".navbar-mobile");
+const body = document.querySelector("body");
 
 openMenu.addEventListener("click", function () {
   mobileNav.classList.toggle("hidden");
+  body.style.overflow = "hidden";
 });
 
 closeMenu.addEventListener("click", function () {
   mobileNav.classList.toggle("hidden");
+  body.style.overflow = "auto";
 });
